54873e129ec29c02c0c24d5fa9fd70ae53cb60dc
David A <dav991@gmail.com>
2023-12-18 03:02:20 +0200

    Add handwired clueboard with rp2040.

---
+++ keyboards/handwired/clueboard/rev1/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF5, 0xA0, 0xB3, 0x96, 0x1B, 0xA5, 0xED, 0xBF}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
