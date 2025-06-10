6bae2a3299077d1d03059141c58af9d618f4f0d6
David A <dav991@gmail.com>
2025-06-11 02:48:35 +0300

    Add cata68 keyboard.

---
+++ keyboards/handwired/cata68/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x35, 0x8A, 0x33, 0x4A, 0x5A, 0xCB, 0x62, 0x7A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
